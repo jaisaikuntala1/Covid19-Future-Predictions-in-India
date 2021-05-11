1a2a2fedc38fe518e61a227b10753adadbc3ab72
Your name here <jaisaikuntala1@gmail.com>
2021-05-11 17:44:44 +0530

    created procfile and requirements.txt

--- Covid19Predictions/settings.py
+++ Covid19Predictions/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2@kzjl(xrskd%7hv-1oxv$aric7$-xw0qjyc!ftlr9888$gk*j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
